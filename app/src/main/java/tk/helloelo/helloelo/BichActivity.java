ef98379f01d2cd6512edd2399171b039bc600931
vpoturaev <viacheslav.poturaev@lazada.com>
2016-07-01 02:12:51 +0700

    working

--- app/src/main/java/tk/helloelo/helloelo/BichActivity.java
+++ app/src/main/java/tk/helloelo/helloelo/BichActivity.java
@@ -1,6 +1,5 @@
 package tk.helloelo.helloelo;
 
-import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,18 +9,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -40,18 +35,6 @@ public class BichActivity extends AppCompatActivity implements AdapterView.OnIte
 
         self = this;
 
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-
-        // Использование собственного шаблона
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        //        R.layout.image_btn_list_item, R.id.label, values);
-
-        // Использование собственного шаблона
-        adapter = new ArrayAdapter<String>(this,
-                R.layout.image_btn_list_item, R.id.label, values);
-
         TextView playerName = (TextView) findViewById(R.id.playerName);
         playerName.setText(HelloEloClient.player.name);
 
@@ -60,8 +43,6 @@ public class BichActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Set up ListView and Adapter
         ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
 
         HelloEloClient.get("games", null, new JsonHttpResponseHandler() {
